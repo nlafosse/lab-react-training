93c9fb968d7539da030981eeff0aa25bd0731e86
Nathalia Lafosse <lafossenathalia@gmail.com>
2021-11-17 01:26:03 -0500

    rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const Rating = (props) => {
-    let stars = ''
-    if (props.children === 0){
-        return <div><p>{stars += '☆☆☆☆☆'}</p></div>
-    }
-    else {
-        return <div><p>{stars = stars + "★".repeat(Math.round(props.children)) + "☆".repeat(5 - Math.round(props.children))}</p></div>
-    }
+    // let stars = ''
+    // return <div><p>{stars = stars + "★".repeat(Math.round(props.children)) + "☆".repeat(5 - Math.round(props.children))}</p></div>
 
-    // let emptyStars = '☆☆☆☆☆'
-    // let filledStars = '★★★★★'
+    
+    let emptyStars = '☆☆☆☆☆'
+    let filledStars = '★★★★★'
+    return <div><p>{filledStars.slice(0, Math.round(props.children)) + emptyStars.slice(0, -(Math.round(props.children)))}</p></div>
+
     // if (props.children === '0'){
     //     return <div><p>{emptyStars}</p></div>
     // }
